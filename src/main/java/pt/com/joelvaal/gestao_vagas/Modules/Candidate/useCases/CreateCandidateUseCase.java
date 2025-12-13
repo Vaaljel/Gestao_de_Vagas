4115267d2577f33ecb4dcad78a0ef557a99dfc8a
Vaaljel <vaaljoel108@gmail.com>
2025-12-13 05:25:12 +0000

    Codificação do candidate

--- src/main/java/pt/com/joelvaal/gestao_vagas/Modules/Candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/pt/com/joelvaal/gestao_vagas/Modules/Candidate/useCases/CreateCandidateUseCase.java
@@ -1,6 +1,7 @@
 package pt.com.joelvaal.gestao_vagas.Modules.Candidate.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import pt.com.joelvaal.gestao_vagas.Modules.Candidate.CandidateEntity;
@@ -10,16 +11,25 @@ import pt.com.joelvaal.gestao_vagas.exception.UserFoundException;
 
 @Service
 public class CreateCandidateUseCase {
-    
+   
+
     @Autowired
     private CandidateRepository candidateRepository;
 
+     
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    
     public CandidateEntity execute(CandidateEntity candidateEntity){
          candidateRepository
         .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
         .ifPresent((user) ->{
             throw new UserFoundException();
         });
+
+        var password = passwordEncoder.encode(candidateEntity.getPassword());
+        candidateEntity.setPassword(password);
+        
      return candidateRepository.save(candidateEntity);
     }
     
--- src/main/java/pt/com/joelvaal/gestao_vagas/exception/UserFoundException.java
+++ src/main/java/pt/com/joelvaal/gestao_vagas/exception/UserFoundException.java
@@ -7,3 +7,4 @@ public class UserFoundException extends RuntimeException{
     }
     
 }
+ 
